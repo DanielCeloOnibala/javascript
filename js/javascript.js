51abfcf6a81706d9f090c6ed402a989c20f0a3f3
DanielCeloOnibala <mamanracingmining22@gmail.com>
2021-08-02 20:52:47 +0700

    modify file javascript

--- js/javascript.js
+++ js/javascript.js
@@ -12,6 +12,13 @@ function pilih()
 		document.getElementById("namarumah").innerHTML="MAWAR";
 		document.isiform.hargarumah.value=130000000;
 	}
+}
+function proses()
+{
+	var x;
+	x = document.getElementById("lamakreditinput").value;
+	document.getElementById("lamakreditoutput").value=x;
+
 	var hargarumah = parseFloat(document.isiform.hargarumah.value)
 	if (isNaN(hargarumah))
 	{
@@ -20,10 +27,18 @@ function pilih()
 	var hasiluangmuka = hargarumah * 0.20;
 	document.isiform.uangmuka.value=+hasiluangmuka;
 
-}
-function proses()
-{
-	var x;
-	x = document.getElementById("lamakreditinput").value;
-	document.getElementById("lamakreditoutput").value=x;
+	var hasilsisa = hargarumah - hasiluangmuka;
+	document.isiform.sisa.value=+hasilsisa;
+
+	var hasilangsuran = hasilsisa / x;
+	document.isiform.angsuran.value=+hasilangsuran;
+
+	var hasil="";
+	for(i=1;i<=x;i++)
+	{
+		angsuran= (hasilsisa-hasilangsuran) ;
+		hasil=hasil+"Angsuran ke-"+i+"	Rp. "+hasilangsuran+" = Rp. "+angsuran+"\n";
+	}
+	document.isiform.ohasil.value=hasil;
+
 }		
